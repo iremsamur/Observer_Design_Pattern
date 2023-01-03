94d85d6a6cbfbd5237a5e53cb9883f72b6bd4cc6
iremsmr <iremsamur129@gmail.com>
2023-01-03 22:52:22 +0300

    Observer Design Pattern Project add

---
+++ UpSchool_Observer_DesignPattern/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UpSchool_Observer_DesignPattern.DAL
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-ISO96UVH\\SQLEXPRESS;Database=DbObserver;integrated security=True;");
+
+
+        }
+        public DbSet<Discount> Discounts { get; set; }
+
+
+    }
+}
---
+++ UpSchool_Observer_DesignPattern/ObserverDesignPattern/IUserObserver.cs
@@ -0,0 +1,9 @@
+﻿using UpSchool_Observer_DesignPattern.DAL;
+
+namespace UpSchool_Observer_DesignPattern.ObserverDesignPattern
+{
+    public interface IUserObserver
+    {
+        void CreateUser(AppUser appUser);
+    }
+}
